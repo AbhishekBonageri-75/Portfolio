da73355e2b4c213e542b640f61f21c14ba4805c7
AFA21SCM21BO <abonageri@hawk.iit.edu>
2022-03-15 13:49:04 -0500

    splash screen added

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 const primaryNav = document.querySelector(".primary-navigation");
 const navToggle = document.querySelector(".mobile-nav-toggle");
+const splash = document.querySelector(".splash");
+
+document.addEventListener("DOMContentLoaded",(e)=>{
+    setTimeout(()=>{
+        splash.classList.add('display-none');
+    },2000)
+})
 
 navToggle.addEventListener("click",() => {
     const visibility = primaryNav .getAttribute("data-visibility");
